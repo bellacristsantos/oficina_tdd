9d49220a9a83204544b1664f826c6c844c1beb86
Isabella Santos <bellacristsantos@gmail.com>
2025-05-11 01:04:43 -0300

    Implementa classe Queue com métodos size, add, peek e dequeue; adiciona testes unitáriosdesafio rolando pedras

--- queue.js
+++ queue.js
@@ -3,8 +3,22 @@ class Queue {
   constructor(){
     this.elements = []
   }
-  
-  // Implementar aqui
+
+  size() {
+    return this.elements.length
+  }
+
+  add(item) {
+    return this.elements.push(item)
+  }
+
+  peek() {
+    return this.elements[0]
+  }
+
+  dequeue() {
+    return this.elements.shift()
+  }
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,37 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add("a")
+    queue.add("b")
+    queue.add("c")
+    queue.add("d")
+    expect(queue.size()).toBe(4)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    queue.add("n")
+    expect(queue.elements).toEqual(["n"])
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add("primeiro")
+    queue.add("segundo")
+    queue.add("terceiro")
+    queue.add("quarto")
+    expect(queue.peek()).toBe("primeiro")
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add("primeiro")
+    queue.add("segundo")
+    queue.add("terceiro")
+    queue.add("quarto")
+    const removed = queue.dequeue()
+    expect(removed).toBe("primeiro")
+    expect(queue.elements).toEqual(["segundo", "terceiro", "quarto"])
   })
 })
